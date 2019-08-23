42598948b4299c4d0530e36525c80be8741caa06
Edie Zhou <edie198@gmail.com>
2019-08-23 02:03:48 -0500

    [io] Initial commit for f1_stats.download_year_data.py

---
+++ f1_stats/download_data_year.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import time
+import os
+import sys
+import argparse
+
+__author__ = "Edie Zhou"
+__credits__ = [""]
+__license__ = "GPL"
+__version__ = "0.0.0"
+__maintainer__ = "Edie Zhou"
+__email__ = "edie.zhou@utexas.edu"
+__status__ = "Development"
+
+def get_inputs():
+    return None
+
+def main():
+    input_0 = get_inputs()
+    return None
