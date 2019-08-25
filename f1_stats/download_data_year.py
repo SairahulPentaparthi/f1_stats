3cb492dba49ab8613c65193fadf12455ea075cf4
Edie Zhou <edie198@gmail.com>
2019-08-24 19:23:59 -0500

    [io] Added .format kwargs, added support for multiple years

--- f1_stats/download_data_year.py
+++ f1_stats/download_data_year.py
@@ -9,20 +9,21 @@ the f1 ergast api.
 """
 
 import argparse
+import datetime as dt
 import json
 import os
-import requests
 import sys
 import time
 
+import requests
 
-__author__ = "Edie Zhou"
-__credits__ = ["Khaled Hemchik: https://github.com/hechmik"]
-__license__ = "GPL"
-__version__ = "0.0.0"
+__author__     = "Edie Zhou"
+__credits__    = ["Khaled Hemchik: https://github.com/hechmik"]
+__license__    = "MIT"
+__version__    = "0.0.1"
 __maintainer__ = "Edie Zhou"
-__email__ = "edie.zhou@utexas.edu"
-__status__ = "Development"
+__email__      = "edie.zhou@utexas.edu"
+__status__     = "Development"
 
 
 def get_inputs():
@@ -45,7 +46,7 @@ def get_inputs():
     return args.years, args.root, args.num_rounds
 
 
-def request_data(base_url, num_rounds, session_name, season = '2018', dataset_root = '/home/ezhou/Documents/dataset/'):
+def request_data(base_url, num_rounds, session, year='2018', dataset_root='/home/ezhou/Documents/dataset/'):
     """
     Purpose:
         Requests data from the online Ergast formula 1 api and dumps it into a user specified directory
@@ -61,9 +62,9 @@ def request_data(base_url, num_rounds, session_name, season = '2018', dataset_ro
         None
     """
     for round in range(1, num_rounds + 1):
-        current_round_data_url = base_url.format(round)
+        current_round_data_url = base_url.format(round=round)
         current_round_data = requests.get(current_round_data_url).json()
-        filename = "{}{}_{}_{}.json".format(dataset_root, int(season), round, session_name)
+        filename = "{}{}_{}_{}.json".format(dataset_root, int(year), round, session)
         print(filename)
         with open(filename, 'w') as f:
             json.dump(current_round_data, f)
@@ -80,28 +81,30 @@ def download_quali_and_race_data(years, root, num_rounds=21):
         Downloads qualifying and race data from a round and saves it into a base root dir
 
     Params:
-        years      (str): List of years to pull data for
+        years     (list): List of year strings to pull data for
         root       (str): User input root dir of dataset
         num_rounds (str): Number of rounds in year
 
     Returns:
         None
     """
-    qualy_url_api = "http://ergast.com/api/f1/2018/{}/qualifying.json"
-    race_url_api = "http://ergast.com/api/f1/2018/{}/results.json"
     for year in years:
+        api_url = "http://ergast.com/api/f1/{year}/{round}/{session}.json"
+        qualy_url = api_url.format(year=year, session='qualifying')
+        race_url = api_url.format(year=year, session='race')
         print("Download qualifying data for {year}".format(year=year))
-        request_data(base_url=qualy_url_api, num_rounds=num_rounds, session_name="qualy",
-                     season=year, dataset_root=root)
+        request_data(base_url=qualy_url, num_rounds=num_rounds, session="qualy",
+                     year=year, dataset_root=root)
         print("Download race data for {year}".format(year=year))
-        request_data(base_url=race_url_api, num_rounds=num_rounds, session_name="race",
-                     season=year, dataset_root=root)
+        request_data(base_url=race_url, num_rounds=num_rounds, session="race",
+                     year=year, dataset_root=root)
 
     return None
 
 
 def main():
     years, root, num_rounds= get_inputs()
+    print(years)
     download_quali_and_race_data(years, root, num_rounds)
 
     return None
